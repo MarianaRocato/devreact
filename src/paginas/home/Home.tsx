36b31ddb540ccb8bf5c3e8efce3e3abd5b047c3b
Mariana Rocato <mariana.rocato_flaibam@hotmail.com>
2023-05-12 11:18:41 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+return(
+    <>
+    	<h1 className="titulo">Olá! Sou a Mari</h1>
+	    <img className="imagem" src="https://t.ctcdn.com.br/AthW2lYFuEnGURVf4_0FtODm-L8=/720x405/smart/filters:format(webp)/i257652.jpeg" alt=' '/>
+    </>
+)
+}
+
+export default Home;
